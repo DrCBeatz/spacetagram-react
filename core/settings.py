17787a923f6780e0500d8d46d2147e52c09fb695
DrCBeatz <brynecarruthers@gmail.com>
2024-02-05 23:45:11 -0500

    update settings.py

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,6 @@ from environs import Env
 env = Env()
 env.read_env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
